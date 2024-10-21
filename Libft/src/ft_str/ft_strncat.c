8131118f8d00da299562f236dee1b7b19662ad1d
RepentedAlex <petitcollin.alessandro@gmail.com>
2024-10-21 22:05:15 +0200

    Added ft_strncat() to Libft

---
+++ Libft/src/ft_str/ft_strncat.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 21:50:39 by apetitco          #+#    #+#             */
+/*   Updated: 2024/10/21 21:50:39 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+{
+	char	*og_dest;
+
+	og_dest = s1;
+	while (*s1)
+		s1++;
+	while (n-- && *src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (og_dest);
+}
--- include/libft.h
+++ include/libft.h
@@ -58,6 +58,7 @@ char	*ft_strdup(const char *s);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
